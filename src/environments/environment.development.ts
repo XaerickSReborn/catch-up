f93db2a9189fe67d4c7adbbfc2621110e089a6b3
XaerickSReborn <u202213358@upc.edu.pe>
2024-09-05 19:43:22 -0500

    chore: configured environment variables.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+// Environment: development
+// This is the development environment configuration file.
+// It contains the configuration settings for the development environment.
+// Note: In real scenarios, this file is not committed to the source control.
+export const environment = {
+  production: false,
+  newsApiKey: 'de576a64a3fa43dc86b24901ad1d36c2'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// Environment: production
+// This is the production environment configuration file.
+// It contains the configuration settings for the production environment.
+// Note: In real scenarios, this file is not committed to the source control.
+export const environment = {
+  production: true,
+  newsApiKey: 'de576a64a3fa43dc86b24901ad1d36c2'
+};
